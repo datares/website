6758f3e75d3e8064643154fa124d0d7dc79a5f25
Arnav Garg <arnavgrg@gmail.com>
2021-04-04 19:29:13 -0700

    Removed warnings

--- src/Components/Consulting.js
+++ src/Components/Consulting.js
@@ -1,7 +1,7 @@
 // Import Components
 import React from 'react';
 import {Header, Container, Image,
-            Grid, Divider, Segment, Icon, GridColumn} from 'semantic-ui-react';
+            Grid, Divider, Segment, Icon} from 'semantic-ui-react';
 
 // Import JS
 import '../Styles/Utils.css';
