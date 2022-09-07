79c6a72282a354212e4240fe115044f304f8a6c4
Colin Curtis <colinpcurtis826@ucla.edu>
2022-09-06 17:58:23 -0700

    edit a bunch of stuff

--- src/Components/DataBlog.js
+++ src/Components/DataBlog.js
@@ -11,6 +11,7 @@ import 'semantic-ui-css/semantic.min.css';
 // Import Images
 import blogImage from '../Assets/DataBlog/blog_image.svg';
 
+
 // Temporarily added articles (in datewise order)
 // Will eventually replace with articles dynamically updated from Medium website
 const Articles = [
@@ -447,7 +448,7 @@ class DataBlog extends React.Component {
     }
     render() {
         return (
-            <div style={{backgroundColor: '#333b65'}}>
+            <div style={{backgroundColor: '#333b65', color: 'white'}}>
 
                 {/* Main Description  */}
                 <div className="Header">
@@ -478,6 +479,47 @@ class DataBlog extends React.Component {
                         <a href="https://ucladatares.medium.com/"> https://ucladatares.medium.com/</a></p>
                     </Container>
                 </div>
+                
+                <div style={{maxWidth: '70%', paddingLeft: '25%'}}>
+                    <Header style={{color: 'white'}}>Data Blog Team FAQ</Header>
+                    <div style={{textAlign: 'left'}}>
+                        <h2>What will I be doing when I join the Data Blog Team?</h2>
+                        <p style={{fontSize: '.4em', paddingLeft: "5%"}}>As a data blog member, </p>
+                        <ul style={{fontSize: '.4em'}}>
+                            <li>
+                            <p>You will be developing a data-driven article on any topic throughout each quarter with a team. Check out articles from past teams: https://medium.com/@ucladatares. </p>
+                            </li>
+                            <li>
+                                <p>
+                                You will have the opportunity to participate in general Data Blog meetings/workshops to develop your skills in data cleaning/sourcing, analysis, visualization, communication, and presentation. 
+                                </p>
+                            </li>
+                            <li>
+                                <p>
+                                You can join social nights throughout the quarter to meet other members in DataRes!
+                                </p>
+                            </li>
+                        </ul>
+                        <h2>I don’t have a lot of experience with programming or working with data. Can I still join?</h2>
+                        <p style={{fontSize: '.4em', paddingLeft: "5%"}}>Yes! We don’t require in depth experience, but we do require a baseline of knowing a bit of R or Python. In the short answer questions of the app, you can demonstrate your prior knowledge through previous personal projects or coursework that you’ve taken (Ex: Statistics 20).</p>
+                        <h2>What does an ideal Data Blog candidate look like?</h2>
+                        <p style={{fontSize: '.4em', paddingLeft: "5%"}}>The most important attributes we’re looking for is enthusiasm and dedication. We want to provide you with a welcoming space to develop your technical skills, so we ask that you are able to actively engage with your team throughout the quarter. We welcome students of all skill levels and backgrounds as long as you have an interest in data analysis, visualization, and/or storytelling!
+                        </p>
+                        <h2>What does the recruitment process look like?</h2>
+                        <p style={{fontSize: '.4em', paddingLeft: "5%"}}>
+                        We require one written application with a few short answer questions about your background and interests. There are no interviews or coding assessments, as we also want to make it an approachable experience for beginners!
+                        </p>
+                        <h2>How competitive is the application process for Data Blog and how can I make my application stand out</h2>
+                        <p style={{fontSize: '.4em', paddingLeft: "5%"}}>
+                        We do not have a hard number cap. The number of people we accept will be dependent on the number of project leads and number of returners from the previous quarter. During Fall quarter, we had a total of 8 teams and 40 active Data Blog members.
+                        </p>
+                        <p style={{fontSize: '.4em', paddingLeft: "5%"}}>
+                        We love to see if you have ideas that you want to turn into a project! Bonus points if you can discuss any specific article topics you are interested in analyzing/writing about in your application.
+                        </p>
+                    </div>
+                    
+                </div>
+
 
                 {/* Ideally, add react search bar */}
 
